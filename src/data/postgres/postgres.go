4d0e352538fe9970929e4836bd4bf6e18c7db255
Rshia Izadyar <arshiaa104@gmail.com>
2024-03-12 17:25:30 +0330

    added transactions

--- src/data/postgres/postgres.go
+++ src/data/postgres/postgres.go
@@ -3,13 +3,12 @@ package postgres
 import (
 	"database/sql"
 	"fmt"
-	"time"
 
 	"github.com/Arshia-Izadyar/Fast-Gopher/src/config"
 	_ "github.com/lib/pq"
 )
 
-var db *sql.DB
+var psqlDb *sql.DB
 
 func ConnectDB(cfg *config.Config) (*sql.DB, error) {
 	var err error
@@ -22,31 +21,24 @@ func ConnectDB(cfg *config.Config) (*sql.DB, error) {
 		cfg.Postgres.DbName,
 	)
 
-	db, err = sql.Open("postgres", cnn)
+	db, err := sql.Open("postgres", cnn)
 	if err != nil {
 		return nil, err
 	}
 
-	db.SetMaxOpenConns(500)
-
-	// Set maximum number of idle connections in the pool.
-	db.SetMaxIdleConns(300)
-	db.SetConnMaxIdleTime(10 * time.Minute)
-	// Set maximum lifetime of a connection.
-	db.SetConnMaxLifetime(5 * time.Minute)
-
 	if err = db.Ping(); err != nil {
 		return nil, err
 	}
+	psqlDb = db
 	return db, nil
 }
 
 func GetDB() *sql.DB {
-	return db
+	return psqlDb
 }
 
 func CloseDB() error {
-	err := db.Close()
+	err := psqlDb.Close()
 	if err != nil {
 		return err
 	}
